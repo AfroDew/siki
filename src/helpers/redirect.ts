4b02a6602f497c6374e0bf3ddda52b6e7d2c7ddf
Godstime Israel <dev.godstime@gmail.com>
2023-07-24 14:26:59 +0000

    info: EC

--- src/helpers/redirect.ts
+++ src/helpers/redirect.ts
@@ -1,21 +1,23 @@
 export function htmxRedirect(url: string | URL, init?: ResponseInit) {
+  const headers = new Headers(init?.headers);
+
+  headers.set("HX-Redirect", url instanceof URL ? url.toString() : url);
+
   return new Response(null, {
     status: 202,
     ...(init ?? {}),
-    headers: {
-      "HX-Redirect": url instanceof URL ? url.toString() : url,
-      ...(init?.headers ?? {}),
-    },
+    headers,
   });
 }
 
 export function redirect(url: string | URL, init?: ResponseInit) {
+  const headers = new Headers(init?.headers);
+
+  headers.set("Location", url instanceof URL ? url.toString() : url);
+
   return new Response(null, {
     ...(init ?? {}),
     status: 302,
-    headers: {
-      "Location": url instanceof URL ? url.toString() : url,
-      ...(init?.headers ?? {}),
-    },
+    headers,
   });
 }
